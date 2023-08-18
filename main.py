393b85866583d0d4c62b06382bc547d5107b4e94
Diego Saavedra <dsaavedra88@gmail.com>
2023-08-17 20:32:19 -0500

    update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 def main():
     name = os.getenv("USERNAME")
-    print(f"Hello, {name} from Github Actions")
+    print(f"Hello, {name} from Github Actions!!!")
 
 if __name__ == "__main__":
     main()
